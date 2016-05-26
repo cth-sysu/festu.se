8cad48bb30bd5d9e0d0bd55616ba5975ec023178
Marcus Hultman <marcus.hultman1@gmail.com>
2016-05-26 19:58:54 +0200

    Frondend files ffs

--- static/public/js/app.js
+++ static/public/js/app.js
@@ -1,1 +1,34 @@
 /* app.js with site functionality */
+angular.module('festu', [
+  'ngRoute'])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+    })
+    .when('/parties', {
+        templateUrl: 'views/parties.html',
+        controller: 'PartiesCtrl'
+    })
+    .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+    })
+    .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+    })
+    .otherwise('/');
+    $locationProvider.html5Mode(true);
+  })
+  .controller('MainCtrl', function() {
+  })
+  .controller('HomeCtrl', function() {
+  })
+  .controller('PartiesCtrl', function() {
+  })
+  .controller('AboutCtrl', function() {
+  })
+  .controller('ContactCtrl', function() {
+  });
