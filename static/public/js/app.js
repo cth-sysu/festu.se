149284bd8e41cf72ac8eaaf931ab88bfccc8cef5
Johan Angséus <angseus@student.chalmers.se>
2016-05-29 18:00:17 +0200

    Working?

--- static/public/js/app.js
+++ static/public/js/app.js
@@ -1,5 +1,5 @@
 /* app.js with site functionality */
-angular.module('festu', ['ngRoute'])
+angular.module('festu', ['ngRoute', 'infinite-scroll', 'ngAnimate'])
   .config(function($routeProvider, $locationProvider) {
     $routeProvider
     .when('/', {
@@ -55,9 +55,10 @@ angular.module('festu', ['ngRoute'])
       }
     };
   })
-  .controller('PartiesCtrl', function($rootScope) {
+  .controller('PartiesCtrl', function($rootScope, $scope) {
     $rootScope.active = 'parties';
-    this.parties = [{
+
+    $scope.parties = [{
       id: 1,
       name: 'Nollkalaset',
       date: new Date(),
@@ -144,6 +145,39 @@ angular.module('festu', ['ngRoute'])
       },
       imageURL: 'http://cffc.se/thumbnail/thumb/42337/big.jpg'
     }];
+
+    $scope.fetchMoreParties = function(){
+      $scope.parties.push({
+        id: 2,
+        name: 'Nollkalaset',
+        date: new Date(),
+        poster: {
+          small: 'http://festu.chs.chalmers.se/images/kalas/original/139.png',
+          large: 'http://festu.chs.chalmers.se/images/kalas/original/139.png',
+        },
+        ticketSale: {
+          sales: [{
+            startTime: new Date(2016, 7, 22, 12, 0, 0),
+            endTime: new Date(2016, 7, 22, 13, 0, 0),
+            locations: [
+              'Teknologgården (Johanneberg)',
+              'Jupiter (Lindholmen)'
+            ],
+            info: 'Chalmerist with valid student ID only\nMax 1+7 tickets/person'
+          }, {
+            startTime: new Date(2016, 7, 23, 12, 0, 0),
+            endTime: new Date(2016, 7, 23, 13, 0, 0),
+            locations: [
+              'Teknologgården (Johanneberg)'
+            ],
+            info: 'Anyone can buy\nMax 8 tickets/person'
+          }],
+          note: 'Reserve for changes, >= 18 years'
+        },
+        imageURL: 'http://cffc.se/thumbnail/thumb/42332/big.jpg'
+      });
+    }
+
   })
   .controller('AboutCtrl', function($rootScope) {
     $rootScope.active = 'about';
