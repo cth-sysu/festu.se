4cdc4a6f803d3f26b80b036c47d94f7bc210b2c3
Johan Angséus <angseus@student.chalmers.se>
2016-06-02 13:42:21 +0200

    Removed script.js

--- static/public/js/script.js
+++
@@ -1,6 +0,0 @@
-(function($){
-  $(document).ready(function(){
-    // Stretch a background on the main page
-    $(".banner-image").backstretch('images/banner.jpg');
-  }); // End document ready
-})(this.jQuery);
