12f5164bb6db97a59a0e511cf0984bada3fa0d86
Marcus Hultman <marcus.hultman1@gmail.com>
2016-08-16 21:49:31 +0200

    mongo-express

--- app/api.js
+++ app/api.js
@@ -16,44 +16,44 @@ var mailTransport = nodemailer.createTransport(sendmailTransport());
 
 var router = express.Router();
 
-function auth(req, res, next) {
-  if (req.isAuthenticated())
-    return next();
-  next('route');
-}
+// function auth(req, res, next) {
+//   if (req.isAuthenticated())
+//     return next();
+//   next('route');
+// }
 
-router.route('/parties')
-  .get(auth, function(req, res, next) {
-    Party.find()
-    .sort('-date')
-    .exec().then(res.json.bind(res), next);
-  })
-  .post(auth, function(req, res, next) {
-    var name = req.body.name;
-    var date = req.body.date;
-    var ticketSaleDate = new Date(req.body.ticketSaleDate);
-    var ticketSaleDate2 = new Date(ticketSaleDate.getTime() + 86400000);
-    new Party({
-      name, date,
-      ticketSale: {
-        sales: [{
-          startTime: ticketSaleDate,
-          endTime: new Date(ticketSaleDate.getTime() + 3600000),
-          locations: [ 'Teknologgården (Johanneberg)', 'Jupiter (Lindholmen)' ],
-          info: 'Chalmerist with valid student ID only\nMax 1+7 tickets/person'
-        }, {
-          startTime: ticketSaleDate2,
-          endTime: new Date(ticketSaleDate2.getTime() + 3600000),
-          locations: [ 'Teknologgården (Johanneberg)' ],
-          info: 'Anyone can buy\nMax 8 tickets/person'
-        }],
-        note: 'Reserve for changes, >= 18 years'
-      }
-    })
-    .save().then(function(party) {
-      res.json(party);
-    }, next);
-  });
+// router.route('/parties')
+//   .get(auth, function(req, res, next) {
+//     Party.find()
+//     .sort('-date')
+//     .exec().then(res.json.bind(res), next);
+//   })
+//   .post(auth, function(req, res, next) {
+//     var name = req.body.name;
+//     var date = req.body.date;
+//     var ticketSaleDate = new Date(req.body.ticketSaleDate);
+//     var ticketSaleDate2 = new Date(ticketSaleDate.getTime() + 86400000);
+//     new Party({
+//       name, date,
+//       ticketSale: {
+//         sales: [{
+//           startTime: ticketSaleDate,
+//           endTime: new Date(ticketSaleDate.getTime() + 3600000),
+//           locations: [ 'Teknologgården (Johanneberg)', 'Jupiter (Lindholmen)' ],
+//           info: 'Chalmerist with valid student ID only\nMax 1+7 tickets/person'
+//         }, {
+//           startTime: ticketSaleDate2,
+//           endTime: new Date(ticketSaleDate2.getTime() + 3600000),
+//           locations: [ 'Teknologgården (Johanneberg)' ],
+//           info: 'Anyone can buy\nMax 8 tickets/person'
+//         }],
+//         note: 'Reserve for changes, >= 18 years'
+//       }
+//     })
+//     .save().then(function(party) {
+//       res.json(party);
+//     }, next);
+//   });
 
 router.route('/parties')
   .get(function(req, res, next) {
@@ -72,51 +72,51 @@ router.route('/parties/next')
     }, next);
   });
 
-router.route('/parties/:party_id')
-  .put(auth, function(req, res, next) {
-    var cffc = req.body.cffc;
-    var image = req.body.image;
-    if (cffc && image) {
-      // Download and store image file
-      var file = fs.createWriteStream('./static/images/parties/' +
-        req.params.party_id + '.jpg');
-      var request = http.get(image, function(response) {
-        response.pipe(file);
-      });
-    }
-    Party.findByIdAndUpdate(req.params.party_id,
-      { $set: { cffc } })
-    .exec().then(function(party) {
-      res.end();
-    }, next);
-  })
-  .delete(auth, function(req, res, next) {
-    Party.findByIdAndRemove(req.params.party_id)
-    .exec().then(function(party) {
-      res.end();
-    }, next);
-  });
+// router.route('/parties/:party_id')
+//   .put(auth, function(req, res, next) {
+//     var cffc = req.body.cffc;
+//     var image = req.body.image;
+//     if (cffc && image) {
+//       // Download and store image file
+//       var file = fs.createWriteStream('./static/images/parties/' +
+//         req.params.party_id + '.jpg');
+//       var request = http.get(image, function(response) {
+//         response.pipe(file);
+//       });
+//     }
+//     Party.findByIdAndUpdate(req.params.party_id,
+//       { $set: { cffc } })
+//     .exec().then(function(party) {
+//       res.end();
+//     }, next);
+//   })
+//   .delete(auth, function(req, res, next) {
+//     Party.findByIdAndRemove(req.params.party_id)
+//     .exec().then(function(party) {
+//       res.end();
+//     }, next);
+//   });
 
 
 // ?year=2015
-router.route('/posts')
-  .get(auth, function(req, res, next) {
-    Post.find().exec().then(res.json.bind(res), next);
-  });
-router.route('/members')
-  .get(auth, function(req, res, next){
-    Member.find()
-    .select('-__v')
-    .populate({ path: 'post', select: 'symbol name' })
-    .sort('-year')
-    .exec().then(function(members){
-      res.json(members);
-    }, next)
-  })
+// router.route('/posts')
+//   .get(auth, function(req, res, next) {
+//     Post.find().exec().then(res.json.bind(res), next);
+//   });
+// router.route('/members')
+//   .get(auth, function(req, res, next){
+//     Member.find()
+//     .select('-__v')
+//     .populate({ path: 'post', select: 'symbol name' })
+//     .sort('-year')
+//     .exec().then(function(members){
+//       res.json(members);
+//     }, next)
+//   })
   // .put()
-  .post(auth, function(req, res, next) {
-    new Member(req.body).save().then(res.json.bind(res), next);
-  })
+  // .post(auth, function(req, res, next) {
+  //   new Member(req.body).save().then(res.json.bind(res), next);
+  // })
   // .delete()
   ;
 
@@ -133,31 +133,31 @@ router.route('/members/current')
   });
 
 // /api/strings/contact_info
-router.route('/strings/:key')
-  .get(function(req, res, next) {
-    StaticString.findOne({ key: req.params.key})
-    .then(function(val) {
-      if (!val) return res.status(404).end();
-      res.json({ value: val.value });
-    }, next);
-  })
-  .post(auth, function(req, res, next) {
-    var key = req.params.key;
-    var value = req.body.value;
-    if (!value) return res.status(400).end();
-    StaticString.findOneAndUpdate({ key }, { key, value },
-      { upsert: true, new: true })
-    .exec().then(function(val) {
-      res.json({ value: val.value });
-    }, next);
-  })
-  .delete(auth, function(req, res, next) {
-    var key = req.params.key;
-    StaticString.findOneAndRemove({ key })
-    .exec(function(val) {
-      res.end();
-    }, next);
-  });
+// router.route('/strings/:key')
+//   .get(function(req, res, next) {
+//     StaticString.findOne({ key: req.params.key})
+//     .then(function(val) {
+//       if (!val) return res.status(404).end();
+//       res.json({ value: val.value });
+//     }, next);
+//   })
+  // .post(auth, function(req, res, next) {
+  //   var key = req.params.key;
+  //   var value = req.body.value;
+  //   if (!value) return res.status(400).end();
+  //   StaticString.findOneAndUpdate({ key }, { key, value },
+  //     { upsert: true, new: true })
+  //   .exec().then(function(val) {
+  //     res.json({ value: val.value });
+  //   }, next);
+  // })
+  // .delete(auth, function(req, res, next) {
+  //   var key = req.params.key;
+  //   StaticString.findOneAndRemove({ key })
+  //   .exec(function(val) {
+  //     res.end();
+  //   }, next);
+  // });
 
 router.post('/contact', function(req, res, next) {
   if (!req.body.mail || !req.body.message) return next(500);
