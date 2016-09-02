677ee0d4009318bdd1f262182ca883ea210a3294
Johan Angséus <angseus@student.chalmers.se>
2016-09-02 16:44:15 +0200

    Started on orv page that allows editing member info from crap web instead of hardcore database.

---
+++ static/orv/js/app.js
@@ -0,0 +1,73 @@
+/* app.js with site functionality */
+angular.module('festu-orv', ['ngRoute', 'ngMaterial'])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+    .when('/', {
+      redirectTo: '/members'
+    })
+    .when('/members', {
+      templateUrl: 'views/members.html',
+      controller: 'MembersCtrl',
+      controllerAs: 'ctrl'
+    })
+    .otherwise('/');
+    $locationProvider.html5Mode({
+      enabled: true,
+    });
+  })
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+    .primaryPalette('red')
+    .accentPalette('purple');
+  })
+  .controller('MembersCtrl', function($rootScope, $http, $location, $mdDialog) {
+    $rootScope.active = 'members';
+    var vm = this;
+    $http.get('/api/members')
+    .then(function(res) {
+      vm.members = res.data;
+    });
+    this.maillist = function(ev) {
+      $mdDialog.show($mdDialog.alert()
+      .title('Maillist')
+      .textContent(this.members
+        .filter(function(member) {
+          return member.mail && member.mail.indexOf('@') >= 0;
+        })
+        .map(function(member) {
+          return member.mail;
+        }).join(', '))
+      .ok('ok').targetEvent(ev));
+    };
+    this.x = function(year) {
+      var now = new Date();
+      return now.getFullYear() - new Date(year,0,1).getFullYear() -
+        (now.getMonth() < 7 ? 1 : 0);
+    };
+    this.edit = function(ev, member) {
+      $mdDialog.show({
+        controller: 'EditMemberCtrl',
+        controllerAs: 'ctrl',
+        templateUrl: '/orv/views/edit_member.html',
+        targetEvent: ev,
+        clickOutsideToClose: true,
+        locals: { member },
+        bindToController: true
+      })
+    };
+  })
+  .controller('EditMemberCtrl', function($rootScope, $http, $mdDialog, member) {
+    $rootScope.active = 'members';
+    var vm = this;
+  
+    this.programmes = [ 'K', 'KfKb', 'Sjö', 'M', 'TD', 'Z', 'E', 'D', 'IT', 'F', 'A', 'V', 'I', 'H' ];
+    this.member = member;
+
+    this.cancel = $mdDialog.cancel;
+    this.save = function(member) {
+      $http.put('/api/members/', { member })
+      .then(function(res) {
+        $mdDialog.hide(member);
+      });
+    };
+});
