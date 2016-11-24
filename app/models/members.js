036b8e6e2cdcded94fbed12cc21aa3ad018b32b0
Marcus Hultman <marcus.hultman1@gmail.com>
2016-11-24 21:38:20 +0100

    orv: enable edit phone

--- app/models/members.js
+++ app/models/members.js
@@ -4,7 +4,8 @@ var MemberSchema = new mongoose.Schema({
   name        : { type: String, required:true},
   post        : { type: mongoose.Schema.ObjectId, ref: 'Post'},
   mail        : { type: String},
-  adress     : { type: String},
+  phone       : { type: String},
+  adress      : { type: String},
   programme   : { name: { type: String}, year: { type: Number} },
   year        : { type: Number},
   description : { type: String}
