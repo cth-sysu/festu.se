6a6de0e01118bea132087685a986831312b687d2
Marcus Hultman <hultman@festu.se>
2018-10-20 12:43:59 +0200

    PosterModal: move poster code to component

---
+++ beta/src/components/PosterModal.vue
@@ -0,0 +1,45 @@
+<template>
+  <transition name="slide-down">
+    <div class="poster-modal" @click="$emit('dismiss')">
+      <img :src="`/images/parties/${poster}_small.jpg`" @error="error = true" v-if="!error">
+      <h3 v-else>No poster available.</h3>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'PosterModal',
+  props: {
+    poster: String
+  },
+  data() {
+    return {
+      error: false,
+    };
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.poster-modal {
+  color: white;
+  position: fixed;
+  max-height: calc(100vh - 80px);
+  top: 80px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  background: rgba(0, 0, 0, .4);
+  padding: 8px;
+  & img { max-height: 100%; }
+}
+.slide-down-enter-active, .slide-down-leave-active {
+  transition: all .2s;
+}
+.slide-down-enter, .slide-down-leave-to {
+  transform: translateY(-100%);
+}
+</style>
--- beta/src/views/Parties.vue
+++ beta/src/views/Parties.vue
@@ -2,29 +2,25 @@
   <div class="wall">
     <Party v-for="party in parties" :key="party._id" :party="party"
         @show-poster="poster = party._id"/>
-    <transition name="slide-down">
-      <div v-if="poster" class="poster" @click="poster = null">
-        <img :src="`/images/parties/${poster}_small.jpg`" @error="error = true" v-if="!error">
-        <h3 v-else>No poster available.</h3>
-      </div>
-    </transition>
+    <PosterModal v-if="poster" :poster="poster" @dismiss="poster = null"/>
   </div>
 </template>
 
 <script>
 import Party from '@/components/Party.vue';
+import PosterModal from '@/components/PosterModal.vue';
 import moment from 'moment';
 
 export default {
   name: 'parties',
   components: {
-    Party
+    Party,
+    PosterModal
   },
   data() {
     return {
       parties: [],
       poster: null,
-      error: false,
     };
   },
   async mounted () {
@@ -40,25 +36,5 @@ export default {
   display: flex;
   flex-wrap: wrap;
   padding: 3px;
-}
-.poster {
-  color: white;
-  position: fixed;
-  max-height: calc(100vh - 80px);
-  top: 80px;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  background: rgba(0, 0, 0, .4);
-  padding: 8px;
-  & img { max-height: 100%; }
-}
-.slide-down-enter-active, .slide-down-leave-active {
-  transition: all .2s;
-}
-.slide-down-enter, .slide-down-leave-to {
-  transform: translateY(-100%);
 }
 </style>
