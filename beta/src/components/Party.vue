bd4351bace12ec0b5f4cb62f25cd6a7fd9134098
Marcus Hultman <hultman@festu.se>
2018-10-20 09:57:30 +0200

    Parties: move Party to component

---
+++ beta/src/components/Party.vue
@@ -0,0 +1,50 @@
+<template>
+  <a class="party" :href="party.cffc">
+    <img :src="`/images/parties/${party._id}.jpg`">
+    <div>{{ party.name }} {{ party.date | date }}</div>
+  </a>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'Party',
+  props: {
+    party: Object
+  },
+  filters: {
+    date: (date) => moment(date).format('YYYY'),
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.party {
+  width: calc(100% / 3);
+  position: relative;
+  box-sizing: border-box;
+  padding: 3px;
+  & img {
+    border-radius: 8px;
+    width: 100%;
+    height: 100%;
+  }
+  & div {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    margin: 3px;
+    padding: 4px 8px;
+    border-radius: 0 8px 0 8px;
+    background: rgba(0, 0, 0, .5);
+    color: white;
+  }
+}
+@media (max-width: 720px) {
+  .party { width: 50%; }
+}
+@media (max-width: 480px) {
+  .party { width: 100%; }
+}
+</style>
--- beta/src/views/Parties.vue
+++ beta/src/views/Parties.vue
@@ -2,19 +2,20 @@
   <div>
     <h2>Previous Kalas</h2>
     <div class="wall">
-      <a v-for="party in parties" class="party" :href="party.cffc">
-        <img :src="`/images/parties/${party._id}.jpg`">
-        <div>{{ party.name }} {{ party.date | date }}</div>
-      </a>
+      <Party v-for="party in parties" :key="party._id" :party="party"/>
     </div>
   </div>
 </template>
 
 <script>
+import Party from '@/components/Party.vue';
 import moment from 'moment';
 
 export default {
   name: 'parties',
+  components: {
+    Party
+  },
   data() {
     return {
       parties: []
@@ -24,9 +25,6 @@ export default {
     const res = await fetch('/api/parties');
     this.parties = await res.json();
   },
-  filters: {
-    date: (date) => moment(date).format('YYYY'),
-  },
 }
 </script>
 
@@ -35,25 +33,5 @@ export default {
   display: flex;
   flex-wrap: wrap;
   padding: 3px;
-}
-.party {
-  flex: 33%;
-  min-width: 240px;
-  margin: 3px;
-  position: relative;
-  & img {
-    border-radius: 8px;
-    width: 100%;
-    height: 100%;
-  }
-  & div {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    padding: 4px 8px;
-    border-radius: 0 8px 0 8px;
-    background: rgba(0, 0, 0, .5);
-    color: white;
-  }
 }
 </style>
