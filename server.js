2ffb7f2663bfff910b7f8e3cde87bcf8418665b8
Marcus Hultman <hultman@festu.se>
2018-10-20 16:45:21 +0200

    re-format server file

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-const express         = require('express');
-const http    		= require('http');
-const bodyParser      = require('body-parser');
-const cookieParser    = require('cookie-parser');
-const session         = require('express-session')
-const passport        = require('passport');
-const path            = require('path');
-const helmet          = require('helmet');
-const mongoose        = require('mongoose');
-const dotenv          = require('dotenv').config()
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session')
+const passport = require('passport');
+const path = require('path');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // Express and DB
-const app   = express();
-const db    = require('./config/db');
+const app = express();
+const db = require('./config/db');
 mongoose.connect(db);
 
 // Helmet for secure HTTP headers
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      styleSrc: ["'self'", 'fonts.googleapis.com', 'fonts.gstatic.com', 'use.fontawesome.com', "'unsafe-inline'"],
-      scriptSrc: ["'self'"],
-      imgSrc: ["'self'", "data:"],
-      fontSrc: ["'self'", 'data:', 'fonts.googleapis.com', 'fonts.gstatic.com', 'use.fontawesome.com'],
-      frameSrc: ["'self'", 'www.facebook.com']
-    }
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["'self'", 'fonts.googleapis.com', 'fonts.gstatic.com', 'use.fontawesome.com', "'unsafe-inline'"],
+    scriptSrc: ["'self'"],
+    imgSrc: ["'self'", "data:"],
+    fontSrc: ["'self'", 'data:', 'fonts.googleapis.com', 'fonts.gstatic.com', 'use.fontawesome.com'],
+    frameSrc: ["'self'", 'www.facebook.com']
+  }
 }))
 
 // Parser middlewares
@@ -33,10 +35,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 +app.set('trust proxy', 1);
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: process.env.NODE_ENV === 'production', httpOnly: true },
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: process.env.NODE_ENV === 'production', httpOnly: true },
 }));
 
 // Passport.js
@@ -45,41 +47,39 @@ app.use(passport.session());
 
 var LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy(
-	function(email, password, callback) {
-    callback(null, email == process.env.ORV_USERNAME && password == process.env.ORV_PASSWORD);
+passport.use(new LocalStrategy(function(email, password, callback) {
+  callback(null, email == process.env.ORV_USERNAME && password == process.env.ORV_PASSWORD);
 }));
 passport.serializeUser(function(user, done) {
-	done(null, user);
-	});
+  done(null, user);
+});
 passport.deserializeUser(function(user, done) {
-	done(null, user);
+  done(null, user);
 });
 
 // Auth
-app.route('/login')
-    .get(function(req, res, next){
-	    res.sendFile(path.join(__dirname, '/static/public', 'login.html'));
-	})
-   .post(passport.authenticate('local', {
-	failureRedirect: '/login?error',
-     successRedirect: '/orv'
-   }));
-app.get('/logout', function(req, res){
-	req.logout();
-    res.redirect('/');
+app.route('/login').get(function(req, res, next) {
+  res.sendFile(path.join(__dirname, '/static/public', 'login.html'));
+}).post(passport.authenticate('local', {
+  failureRedirect: '/login?error',
+  successRedirect: '/orv'
+}));
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 function auth(req, res, next) {
-    if (req.isAuthenticated()){
-    	return next();
-    } else {
-    	res.redirect("/login");
-    }
+  if (req.isAuthenticated()){
+    return next();
+  } else {
+    res.redirect("/login");
+  }
 }
 
 // API
-var api = require('./app/api');
+const api = require('./app/api');
 app.use('/api', api);
 
 // Static
@@ -87,11 +87,9 @@ app.use(express.static(__dirname + '/static/misc', { index: false }));
 app.use(express.static(__dirname + '/static/beta', { index: false }));
 app.use('/orv', auth, express.static(__dirname + '/static/orv', { index: false }));
 
-app.use('/images',
-  express.static(__dirname + '/static/images', { index: false }),
-  function(req, res, next) {
-    res.status(404).end();
-  });
+app.use('/images', express.static(__dirname + '/static/images', { index: false }), function(req, res, next) {
+  res.status(404).end();
+});
 
 // Routes
 app.get('/orv*', auth, function(req, res) {
