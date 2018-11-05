f63e661ad2300aca0b1be9990b30304c3acd748f
Marcus Hultman <hultman@festu.se>
2018-11-05 22:03:44 +0100

    config: pass auth token to dev server proxy

--- beta/vue.config.js
+++ beta/vue.config.js
@@ -1,10 +1,17 @@
+require('dotenv').config({
+  path: require('path').resolve(process.cwd(), '..', '.env')
+});
+
 module.exports = {
   lintOnSave: false,
   outputDir: '../static/beta',
   devServer: {
     proxy: {
       '/api': {
-        target: 'https://festu.se'
+        target: 'https://festu.se',
+        headers: {
+          authorization: `Bearer ${process.env.TOKEN}`
+        }
       },
       '/images': {
         target: 'https://festu.se'
