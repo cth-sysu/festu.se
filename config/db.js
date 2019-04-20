586e97f9f9fc8e575f4632ff5830cd729944354d
Marcus Hultman <hultman@festu.se>
2019-04-20 19:16:46 +0200

    config: fix wrong port variable

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 const credentials = `${process.env.DB_USER}:${process.env.DB_PASSWORD}`;
-const server = `${process.env.DB_HOST}:${process.env.PORT}`;
+const server = `${process.env.DB_HOST}:${process.env.DB_PORT}`;
 const authSource = process.env.DB_AUTH_NAME || process.env.DB_NAME;
 module.exports = `mongodb://${credentials}@${server}/${process.env.DB_NAME}?authSource=${authSource}`;
